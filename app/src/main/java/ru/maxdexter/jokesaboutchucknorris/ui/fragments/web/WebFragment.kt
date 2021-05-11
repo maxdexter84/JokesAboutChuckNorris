6d98dab27d075436114987fcf7b7e4c463008166
maxdexter <Maximusnew84!>
2021-05-11 11:57:52 +0300

    add WebFragment.kt and WebViewModel.kt

---
+++ app/src/main/java/ru/maxdexter/jokesaboutchucknorris/ui/fragments/web/WebFragment.kt
@@ -0,0 +1,24 @@
+package ru.maxdexter.jokesaboutchucknorris.ui.fragments.web
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.maxdexter.jokesaboutchucknorris.R
+
+class WebFragment : Fragment() {
+
+
+
+    private lateinit var viewModel: WebViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.web_fragment, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/ru/maxdexter/jokesaboutchucknorris/ui/fragments/web/WebViewModel.kt
@@ -0,0 +1,7 @@
+package ru.maxdexter.jokesaboutchucknorris.ui.fragments.web
+
+import androidx.lifecycle.ViewModel
+
+class WebViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
